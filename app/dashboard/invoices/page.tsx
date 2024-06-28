9ed1656fed377a7ba76f4b4f5c82cd2eb4b0503a
lex.role <lex.role@icloud.com>
2024-06-28 12:04:51 +0200

    Chapter 12

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,9 +5,21 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { fetchInvoicesPages } from '@/app/lib/data';
 
 
-export default function Page() {
+export default function Page({
+    searchParams,
+}: {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    };
+    }){ 
+        const query = searchParams?.query || '';
+        const currentPage = Number(searchParams?.page) || 1;
+        const totalPages = fetchInvoicesPages(query);
+    
     return (
         <div className='w-full'>
             <div className='flex w-full items-center justify-between'>
@@ -17,11 +29,11 @@ export default function Page() {
                 <Search placeholder='Search invoices...' />
                 <CreateInvoice />
             </div>
-            {/* <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>   
+            <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>   
                 <Table query={query} currentPage={currentPage} />
-            </Suspense> */}
+            </Suspense>
             <div className="mt-5 flex w-full justify-center">
-                {/* <Pagination totalPages={totalPages} /> */}
+                <Pagination totalPages={totalPages} />
             </div>
         </div>
     );
